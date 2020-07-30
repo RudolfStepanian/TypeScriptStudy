28ec4b696b398fab70d33b0c3ed7959c7dcbae5c
lilitabrahamyan <lilitdev123@gmail.com>
2020-07-30 13:53:51 +0400

    First commit

---
+++ Types.ts
@@ -0,0 +1,17 @@
+const isFetching:boolean = false;
+const isLoading:boolean = true;
+
+const int:number = 1;
+const float:number = 1.85;
+const num:number = 1e5;
+
+
+const message:string = "TypeScript";
+
+
+const numberArray:number[] = [1,2,3];
+const numberArray2:Array<number> = [1,2,3];
+
+const words: string[] = ["hello", "world"];
+
+const contact: [string,number] = ["Abcd", 60];
